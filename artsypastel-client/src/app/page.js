22d687d3760b131f6b4287b44e57cdacf7c49609
Govind-05 <govindbhardwaj501@gmail.com>
2024-04-22 23:37:50 +0530

    add responsiveness to services section

--- artsypastel-client/src/app/page.js
+++ artsypastel-client/src/app/page.js
@@ -94,9 +94,9 @@ export default function Home() {
         <Slideshow />
       </div>
 
-      <div className={`flex ${serviceCardsShow&&"bg-red-400 transition-all duration-1000"}`} onMouseLeave={()=>setServiceCardsShow(false)}>
-        <div className='w-6/12 flex items-center justify-center relative m-4'>
-          <BackgroundImageCard heading={"Customization"} subheading={"We will customise your belongings"}  classes={
+      <div className={`flex flex-col xl:flex-row ${serviceCardsShow&&"bg-red-400 transition-all duration-1000"}`} onMouseLeave={()=>setServiceCardsShow(false)}>
+        <div className='w-6/12 hidden items-center justify-center relative m-4 xl:flex'>
+          <BackgroundImageCard heading={"Customize"} subheading={"We will customise your belongings"}  classes={
             !serviceCardsShow?`bg-[url("/images/background.jpg")] bg-cover shadow-[-5px_5px_11px_0px_rgba(0,0,0,0.5)] absolute left-2/4 bg-[#b1fd74] top-2/4 -rotate-[24deg] -translate-x-16 -translate-y-[calc(50%+4rem)]`
             :
             `bg-[url("/images/background.jpg")] bg-cover shadow-[-5px_5px_11px_0px_rgba(0,0,0,0.5)] hover:translate-y-[5px] absolute left-0 bg-[#b1fd74] top-0 transition-all duration-1000`
@@ -107,7 +107,19 @@ export default function Home() {
           <BackgroundImageCard heading={"Cards"} subheading={"Prettu much related to cards"} classes={!serviceCardsShow?'bg-[url("/images/background.jpg")] bg-cover shadow-[-5px_5px_11px_0px_rgba(0,0,0,0.5)] absolute left-2/4 bg-[#fd74d6] top-2/4 -rotate-12 -translate-x-8 -translate-y-[calc(50%+2rem)]':`bg-[url("/images/background.jpg")] bg-cover shadow-[-5px_5px_11px_0px_rgba(0,0,0,0.5)] hover:-translate-y-[calc(100%+5px)] absolute left-0 bg-[#fd74d6] top-full -translate-y-[100%] transition-all duration-1000`} />
           <BackgroundImageCard heading={"Coming soon..."} subheading={"More services in the future"} classes={!serviceCardsShow?'bg-[url("/images/background.jpg")] bg-cover shadow-[-5px_5px_11px_0px_rgba(0,0,0,0.5)] absolute left-2/4 bg-[#fd7474] top-2/4 -rotate-6 -translate-x-4 -translate-y-[calc(50%+1rem)]':`bg-[url("/images/background.jpg")] bg-cover shadow-[-5px_5px_11px_0px_rgba(0,0,0,0.5)] hover:-translate-y-[calc(100%+5px)] absolute left-full -translate-x-[100%] bg-[#fd7474] top-full -translate-y-[100%] transition-all duration-1000`} />
         </div>
-        <div className='w-6/12 flex justify-center'>
+
+        <div className='text-center pt-5 pb-3 text-2xl font-bold xl:hidden'>
+          OUR SERVICES
+        </div>    
+        <div className='flex flex-wrap justify-around xl:hidden'>
+          <BackgroundImageCard heading={"Customize"} subheading={"We will customise your belongings"} classes={'m-2 bg-[url("/images/background.jpg")] bg-cover shadow-[-5px_5px_11px_0px_rgba(0,0,0,0.5)] bg-[#b1fd74] w-28 h-40'}/>
+          <BackgroundImageCard heading={"Portraits"} subheading={"We will deliver your portraits."} classes={'m-2 bg-[url("/images/background.jpg")] bg-cover shadow-[-5px_5px_11px_0px_rgba(0,0,0,0.5)] bg-[#b1fd74] w-28 h-40'}/>
+          <BackgroundImageCard heading={"Special Requests"} subheading={"We would love to hear more about your ideas."} classes={'m-2 bg-[url("/images/background.jpg")] bg-cover shadow-[-5px_5px_11px_0px_rgba(0,0,0,0.5)] bg-[#b1fd74] w-28 h-40'}/>
+          <BackgroundImageCard heading={"Cards"} subheading={"Prettu much related to cards"} classes={'m-2 bg-[url("/images/background.jpg")] bg-cover shadow-[-5px_5px_11px_0px_rgba(0,0,0,0.5)] bg-[#b1fd74] w-28 h-40'}/>
+          <BackgroundImageCard heading={"Coming soon..."} subheading={"More services in the future"} classes={'m-2 bg-[url("/images/background.jpg")] bg-cover shadow-[-5px_5px_11px_0px_rgba(0,0,0,0.5)] bg-[#b1fd74] w-28 h-40'}/>
+        </div>
+
+        <div className='w-6/12 justify-center hidden xl:flex'>
         <Image src="/images/services-artist.png" alt="art images" width={512} height={512} />
         </div>
       </div>
